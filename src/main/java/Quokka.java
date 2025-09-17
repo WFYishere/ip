79a382eb5016a33dd640e80b3913643c16b126ac
WFYishere <wenfanyu2005@hotmail.com>
2025-09-17 22:18:56 +0800

    Add support for echo

--- src/main/java/Quokka.java
+++ src/main/java/Quokka.java
@@ -1,20 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Quokka {
-    public static void main(String[] args) {
+    private static final String LINE = "____________________________________________________________";
+
+    public static void main(String[] args) throws IOException {
         printGreeting();
+        runEchoLoop();
         printGoodbye();
     }
 
     private static void printGreeting() {
-        String line = "____________________________________________________________";
-        System.out.println(line);
+        System.out.println(LINE);
         System.out.println(" Hello! I'm Quokka");
         System.out.println(" What can I do for you?");
-        System.out.println(line);
+        System.out.println(LINE);
+    }
+
+    private static void runEchoLoop() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        while ((input = br.readLine()) != null) {
+            if (input.equals("bye")) {
+                break;
+            }
+            printEcho(input);
+        }
+    }
+
+    private static void printEcho(String message) {
+        System.out.println(LINE);
+        System.out.println(" " + message);
+        System.out.println(LINE);
     }
 
     private static void printGoodbye() {
-        String line = "____________________________________________________________";
+        System.out.println(LINE);
         System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(LINE);
     }
 }
