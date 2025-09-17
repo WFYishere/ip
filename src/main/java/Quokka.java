5b28cd4fbfb32b18daf8e18c23b5cc087cc1561a
WFYishere <wenfanyu2005@hotmail.com>
2025-09-17 22:39:07 +0800

    Add support for marking tasks done/undone

--- src/main/java/Quokka.java
+++ src/main/java/Quokka.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 public class Quokka {
     private static final String LINE = "____________________________________________________________";
     private static final int MAX_TASKS = 100;
-    private static final String[] tasks = new String[MAX_TASKS];
+    private static final Task[] tasks = new Task[MAX_TASKS];
     private static int size = 0;
 
     public static void main(String[] args) throws IOException {
@@ -22,17 +22,23 @@ public class Quokka {
                 break;
             } else if (input.equals("list")) {
                 printList();
+            } else if (input.startsWith("mark ")) {
+                int idx = Integer.parseInt(input.substring(5)) - 1;
+                markTask(idx);
+            } else if (input.startsWith("unmark ")) {
+                int idx = Integer.parseInt(input.substring(7)) - 1;
+                unmarkTask(idx);
             } else {
                 addTask(input);
             }
         }
     }
 
-    private static void addTask(String task) {
+    private static void addTask(String description) {
         if (size < MAX_TASKS) {
-            tasks[size++] = task;
+            tasks[size++] = new Task(description);
             System.out.println(LINE);
-            System.out.println(" added: " + task);
+            System.out.println(" added: " + description);
             System.out.println(LINE);
         } else {
             System.out.println(LINE);
@@ -43,13 +49,31 @@ public class Quokka {
 
     private static void printList() {
         System.out.println(LINE);
+        System.out.println(" Here are the tasks in your list:");
         for (int i = 0; i < size; i++) {
-            System.out.println(" " + (i + 1) + ". " + tasks[i]);
-        }
-        if (size == 0) {
-            System.out.println(" (no tasks yet)");
+            System.out.println(" " + (i + 1) + "." + tasks[i]);
         }
         System.out.println(LINE);
+    }
+
+    private static void markTask(int idx) {
+        if (idx >= 0 && idx < size) {
+            tasks[idx].markAsDone();
+            System.out.println(LINE);
+            System.out.println(" Nice! I've marked this task as done:");
+            System.out.println("   " + tasks[idx]);
+            System.out.println(LINE);
+        }
+    }
+
+    private static void unmarkTask(int idx) {
+        if (idx >= 0 && idx < size) {
+            tasks[idx].markAsNotDone();
+            System.out.println(LINE);
+            System.out.println(" OK, I've marked this task as not done yet:");
+            System.out.println("   " + tasks[idx]);
+            System.out.println(LINE);
+        }
     }
 
     private static void printGreeting() {
